fbd59006f3f03597b0741bcdbdcb27f4ddf29524
usman_rizwan <usmanrizwan771@gmail.com>
2024-01-14 16:09:04 +0500

    Scrroll animation added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,68 +1,71 @@
-import React from 'react';
-import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-} from 'mdb-react-ui-kit';
-import Product1 from "../assets/images/product1.jpg"
-import Product2 from "../assets/images/product2.jpg"
-import Product3 from "../assets/images/product3.jpg"
-import Product4 from "../assets/images/product4.jpg"
-import Product5 from "../assets/images/product5.jpg"
-import Product6 from "../assets/images/product6.jpg"
-import Product7 from "../assets/images/product7.jpg"
-import Product8 from "../assets/images/product8.jpg"
-import Product9 from "../assets/images/product9.jpg"
+import React, { useEffect } from "react";
+import { MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Product1 from "../assets/images/product1.jpg";
+import Product2 from "../assets/images/product2.jpg";
+import Product3 from "../assets/images/product3.jpg";
+import Product5 from "../assets/images/product5.jpg";
+import Product8 from "../assets/images/product8.jpg";
+import Product9 from "../assets/images/product9.jpg";
 
 export default function Gallery() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with a default duration of 1000ms
+    AOS.refresh(); // Refresh AOS when the component mounts
+  }, []);
+
   return (
-    <div className='container'>
-    <MDBRow>
-      <MDBCol lg={4} md={12} className='mb-4 mb-lg-0'>
+    <div className="container">
+      <div className="d-flex justify-content-center items-center">
+        <h1 style={{color:"#c3b5f3"}}>Clout Gallery</h1>
+      </div>
+      <MDBRow>
+        <MDBCol lg={4} md={12} className="mb-4 mb-lg-0">
+          <div data-aos="zoom-out-up">
+            <img
+              src={Product2}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Boat on Calm Water"
+            />
+            <img
+              src={Product3}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Wintry Mountain Landscape"
+            />
+          </div>
+        </MDBCol>
 
-        <img
-          src={Product2}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Boat on Calm Water'
-        />
+        <MDBCol lg={4} className="mb-4 mb-lg-0">
+          <div data-aos="zoom-out-down">
+            <img
+              src={Product1}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Mountains in the Clouds"
+            />
+            <img
+              src={Product5}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Boat on Calm Water"
+            />
+          </div>
+        </MDBCol>
 
-
-        <img
-          src={Product3}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Wintry Mountain Landscape'
-        />
-      
-      </MDBCol>
-
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-         src={Product1}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Mountains in the Clouds'
-        />
-
-        <img
-        src={Product5}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Boat on Calm Water'
-        />
-      </MDBCol>
-
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-         src={Product9}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
-
-        <img
-         src={Product8}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-    </MDBRow>
+        <MDBCol lg={4} className="mb-4 mb-lg-0">
+          <div data-aos="zoom-out-right">
+            <img
+              src={Product9}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Waves at Sea"
+            />
+            <img
+              src={Product8}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Yosemite National Park"
+            />
+          </div>
+        </MDBCol>
+      </MDBRow>
     </div>
   );
 }
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,7 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Image, Space } from 'antd';
 import {
   DownloadOutlined,
@@ -17,8 +19,6 @@ const images = [
  Product4,
   Product6,
   Product7,
-
-  
 ];
 
 const Product = () => {
@@ -36,11 +36,15 @@ const Product = () => {
         link.remove();
       });
   };
-
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with a default duration of 1000ms
+    AOS.refresh(); // Refresh AOS when the component mounts
+  }, []);
   return (
+    
     <div className="product-container">
       {images.map((src, index) => (
-        <div key={index} className="image-container">
+        <div key={index} className="image-container" data-aos="fade-left">
           <Image
             width={300} // Set the width to 500px
            
